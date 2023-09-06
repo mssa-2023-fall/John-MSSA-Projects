e782a4de39906aaddcc5cad386912aa1b590ab71
coubs <john.mccoubrie15@gmail.com>
2023-09-06 11:21:32 -0400

    Update completed lab4

--- DSA/DSA-Lab1-1/DSA-Lab1-1Test/ProteinSelection.cs
+++ DSA/DSA-Lab1-1/DSA-Lab1-1Test/ProteinSelection.cs
@@ -13,21 +13,35 @@ namespace DSA_Lab1_1Test
             menu = new RestaurantMenu();
         }
 
-
         [TestMethod]
-        [DataRow("Beef", "Hamburger")]
+        [DataRow("Beef", "hamburger")]
         [DataRow("Pepperoni", "pizza")]
         [DataRow("Tofu", "Tofu Fried Rice")]
+        [DataRow("BEEF", "hamburger")]
+        [DataRow("PEPPERoni", "pizza")]
+        [DataRow("tofu", "Tofu Fried Rice")]
         public void TestWithExpectedProteinTypeShouldReturnCorrespondingDishes(string proteinChoices, string menuItem)
         {
             string dish = menu.GetDishRecommendation(proteinChoices);
             Assert.AreEqual(menuItem, dish, true);
         }
+
         [TestMethod]
-        public void TestWith_UNExpectedProteinTypeShouldReturnMessageProteinNotAvailable()
+        public void TestWith_UNExpected_ProteinTypeShouldReturnMessageProteinNotAvailable()
         {
             string dish = menu.GetDishRecommendation("fish");
             Assert.AreEqual("That protein is not available.", dish, true);
+        }
+        public void TestConsoleOutput()
+        {
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo("C:\\Users\\victor\\source\\repos\\Chinjila\\DSA\\DSA-Lab1-1\\RestaurantApp\\bin\\Debug\\net6.0\\RestaurantApp.exe");
+            var inputStream = new StreamWriter(new MemoryStream(new byte[100]));
+            p.StartInfo.RedirectStandardInput = true;
+            var outputStream = p.StandardOutput;
+            var outputLine = outputStream.ReadLine();
+
+            Assert.AreEqual(outputLine, "Hello, World!", true);
         }
     }
 }
