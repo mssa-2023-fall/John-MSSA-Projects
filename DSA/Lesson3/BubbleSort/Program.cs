cb0584b49adc0bbc52888ffb9b6c02b7d725ecfb
coubs <john.mccoubrie15@gmail.com>
2023-09-07 10:46:02 -0400

    Update class structure

--- DSA/Lesson3/BubbleSort/Program.cs
+++ DSA/Lesson3/BubbleSort/Program.cs
@@ -1,42 +1,44 @@
-﻿
-
-static void Main(string[] args)
+﻿class Program
 {
-    int[] examScores = { 95, 54, 87, 82, 94, 22, 75 };
-
-    Console.WriteLine("Here are the exam scores before sorting: ");
-    PrintArray(examScores);
-
-    Console.WriteLine("Here are the exam scores after the bubble sort");
-    BubbleSort(examScores);
-
-    Console.WriteLine("Space complexity is O(1) for the bubble sort");
-}
-
-static void BubbleSort(int[] arr)
-{
-    int n = arr.Length;
-    
-    for(int i = 0; i < n - 1; i++)
+    static void Main(string[] args)
     {
-        for(int j = 0; j < n - 1; j++)
+        int[] examScores = { 95, 54, 87, 82, 94, 22, 75 };
+
+        Console.WriteLine("Here are the exam scores before sorting: ");
+        PrintArray(examScores);
+
+        Console.WriteLine("Here are the exam scores after the bubble sort");
+        BubbleSort(examScores);
+
+        Console.WriteLine("Space complexity is O(1) for the bubble sort");
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+
+        for (int i = 0; i < n - 1; i++)
         {
-            if (arr[j] > arr[j + 1])
+            for (int j = 0; j < n - 1; j++)
             {
-                //Swap the elements
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+                if (arr[j] > arr[j + 1])
+                {
+                    //Swap the elements
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
             }
         }
     }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var score in arr)
+        {
+            Console.Write(score + " ");
+        }
+        Console.WriteLine();
+    }
 }
 
-static void PrintArray(int[] arr)
-{
-    foreach(var score in arr)
-    {
-        Console.Write(score + " ");
-    }
-    Console.WriteLine();
-}
