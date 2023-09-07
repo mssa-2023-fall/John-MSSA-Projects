0ae956f338393b747f8b22d50b7c64a00dc088ad
coubs <john.mccoubrie15@gmail.com>
2023-09-07 10:41:56 -0400

    Bubble sort algorithm

---
+++ DSA/Lesson3/BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+static void Main(string[] args)
+{
+    int[] examScores = { 95, 54, 87, 82, 94, 22, 75 };
+
+    Console.WriteLine("Here are the exam scores before sorting: ");
+    PrintArray(examScores);
+
+    Console.WriteLine("Here are the exam scores after the bubble sort");
+    BubbleSort(examScores);
+
+    Console.WriteLine("Space complexity is O(1) for the bubble sort");
+}
+
+static void BubbleSort(int[] arr)
+{
+    int n = arr.Length;
+    
+    for(int i = 0; i < n - 1; i++)
+    {
+        for(int j = 0; j < n - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                //Swap the elements
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+static void PrintArray(int[] arr)
+{
+    foreach(var score in arr)
+    {
+        Console.Write(score + " ");
+    }
+    Console.WriteLine();
+}
---
+++ DSA/Lesson3/Program.cs
@@ -0,0 +1,55 @@
+﻿
+Console.WriteLine(Fib(5));
+Console.WriteLine(Factorial(5));
+
+foreach(var item in FibWithoutRecursion(100))
+{
+    Console.WriteLine(item);
+    if (item < 0) break;
+}
+
+foreach (var item in FibWithoutRecursionAndYieldReturn(100))
+{
+    Console.WriteLine(item);
+    if (item < 0) break;
+}
+
+static int Fib (int input)
+{
+    if(input == 0) return 0;
+    if(input == 1) return 1;
+
+    return Fib(input - 1) + Fib(input - 2);
+}
+
+static int Factorial(int input)
+{
+    if (input == 0) return 1;
+    return input * Factorial(input - 1);
+}
+
+static int[] FibWithoutRecursion(int input)
+{
+    int[] fibSeries = new int[input + 1];
+    fibSeries[0] = 0;
+    fibSeries[1] = 1;
+
+    for(int i = 2; i <=input; i++)
+    {
+        fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+    }
+    Console.WriteLine("Entire resultset are computed");
+    return fibSeries;
+}
+
+static IEnumerable<int> FibWithoutRecursionAndYieldReturn(int input)
+{
+    int[] fibSeries = new int[input + 1];
+    fibSeries[0] = 0;
+    fibSeries[1] = 1;
+    for(int i = 2; i <= input; i++)
+    {
+        fibSeries[i] = fibSeries[i - 1] + fibSeries[i - 2];
+        yield return fibSeries[i];
+    }
+}
