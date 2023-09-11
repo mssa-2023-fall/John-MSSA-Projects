1bdf324230f6bdbeb0408231ac9a158eb7a637f7
coubs <john.mccoubrie15@gmail.com>
2023-09-11 15:16:13 -0400

    Lab 5

---
+++ DSA/Lesson3/LinkedList/UnitTest1/UnitTest1.cs
@@ -0,0 +1,215 @@
+using LinkedList;
+
+namespace LinkedListTest
+{
+    [TestClass]
+    public class LinkedListTest
+    {
+        [TestMethod]
+        public void DefaultConstructorCreatesEmptyLL()
+        {
+            var testLL = new ChinLinkedList<int>();
+            Assert.AreEqual(0, testLL.Count);
+            Assert.IsNull(testLL.Head);
+            Assert.IsNull(testLL.Tail);
+        }
+        [TestMethod]
+        public void EmptyLLCallingRemoveShouldThrowInvalidOperationException()
+        {
+            var testLL = new ChinLinkedList<int>();
+            Assert.ThrowsException<InvalidOperationException>(() => testLL.RemoveLast(), "Remove method on empty list did not throw an exception");
+            Assert.ThrowsException<InvalidOperationException>(() => testLL.RemoveFirst(), "Remove method on empty list did not throw an exception");
+            Assert.ThrowsException<InvalidOperationException>(() => testLL.RemoveAt(0), "Remove method on empty list did not throw an exception");
+
+        }
+
+        [TestMethod]
+        public void ParameterizedConstructorShouldPopulateTheHeadAndTailNode()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            Assert.AreEqual(1, testLL.Count);
+            Assert.IsNotNull(testLL.Head);
+            Assert.IsNotNull(testLL.Tail);
+            Assert.AreSame(testLL.Head, initialNode);
+            Assert.AreSame(testLL.Tail, initialNode);
+        }
+
+        [TestMethod]
+        public void AddFirstShouldReplaceHeadNodeAndCreateLinkToOldHead()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            var addFirstNode = new ChinNode<int>(10);
+
+            testLL.AddFirst(addFirstNode);
+
+            Assert.AreEqual(2, testLL.Count);
+            Assert.IsNotNull(testLL.Head);
+            Assert.IsNotNull(testLL.Tail);
+            Assert.AreSame(testLL.Head, addFirstNode);
+            Assert.AreSame(testLL.Tail, initialNode);
+            Assert.AreEqual(10, testLL.Head.Content);
+            Assert.AreEqual(5, testLL.Tail.Content);
+        }
+
+        [TestMethod]
+        public void AddLastShouldReplaceHeadNodeAndCreateLinkToOldHead()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            var addLastNode = new ChinNode<int>(10);
+
+            testLL.AddLast(addLastNode);
+
+            Assert.AreEqual(2, testLL.Count);
+            Assert.IsNotNull(testLL.Head);
+            Assert.IsNotNull(testLL.Tail);
+            Assert.AreSame(testLL.Head, initialNode);
+            Assert.AreSame(testLL.Tail, addLastNode);
+            Assert.AreEqual(5, testLL.Head.Content);
+            Assert.AreEqual(10, testLL.Tail.Content);
+        }
+
+        [TestMethod]
+        public void ClearMethodShouldReturnEmptyLinkedList()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            var addLastNode = new ChinNode<int>(10);
+            testLL.AddLast(addLastNode);
+
+            testLL.Clear();
+            Assert.AreEqual(0, testLL.Count);
+            Assert.IsNull(testLL.Head);
+            Assert.IsNull(testLL.Tail);
+        }
+
+
+        [TestMethod]
+        public void FindAllMethodShouldReturnNullIfNotFound()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            //finding nonexistant value
+            INode<int>[] result = testLL.FindAll(1);
+
+
+            Assert.AreEqual(0, result.Length);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void FindAllMethodShouldReturnOne()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            //finding nonexistant value
+            INode<int>[] result = testLL.FindAll(5);
+
+
+            Assert.AreEqual(1, result.Length);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result[0].Content, 5);
+
+        }
+
+        [TestMethod]
+        public void FindAllMethodShouldReturnMany()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            testLL.AddFirst(new ChinNode<int>(5));
+            //finding nonexistant value
+            INode<int>[] result = testLL.FindAll(5);
+
+            Assert.AreEqual(2, result.Length);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result[0].Content, 5);
+            Assert.AreEqual(result[1].Content, 5);
+        }
+
+        [TestMethod]
+        public void FindOneMethodShouldReturnExactlyOneEvenIfThereAreMultipleMatches()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            testLL.AddFirst(new ChinNode<int>(5));
+            //finding nonexistant value
+            INode<int>? result = testLL.FindFirst(5);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content, 5);
+        }
+        [TestMethod]
+        public void FindOneMethodShouldReturnNullWhenThereAreNoMatch()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            testLL.AddFirst(new ChinNode<int>(5));
+            //finding nonexistant value
+            INode<int>? result = testLL.FindFirst(8);
+            Assert.IsNull(result);
+        }
+        [TestMethod]
+        public void FindOneMethodShouldReturnOneWhenThereIsOneMatch()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+            testLL.AddFirst(new ChinNode<int>(5));
+            //finding nonexistant value
+            INode<int>? result = testLL.FindFirst(7);
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.Content, 7);
+        }
+
+        [TestMethod]
+        public void RemoveFirstShouldRemoveHeadAndMakeSecondNodeTheHead()
+        {
+            //Arrange
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+            testLL.AddFirst(new ChinNode<int>(6));
+            testLL.AddFirst(new ChinNode<int>(7));
+
+            testLL.RemoveFirst();
+            Assert.AreEqual(2, testLL.Count);
+            Assert.AreEqual(6, testLL.Head?.Content);
+            Assert.AreEqual(5, testLL.Tail?.Content);
+        }
+
+        [TestMethod]
+        public void RemoveFirstShouldReturnEmptyLinkedListWhenThereIsOnlyOneNode()
+        {
+            var initialNode = new ChinNode<int>(5);
+            var testLL = new ChinLinkedList<int>(initialNode);
+
+            testLL.RemoveFirst();
+
+            Assert.AreEqual(0, testLL.Count);
+            Assert.IsNull(testLL.Head);
+            Assert.IsNull(testLL.Tail);
+        }
+
+
+        [TestMethod]
+        public void RemoveFirstShouldThrowExceptionIfLinkedListIsEmpty()
+        {
+            var testLL = new ChinLinkedList<int>();
+
+            Assert.ThrowsException<InvalidOperationException>(() => testLL.RemoveFirst(), "We think, if one attempts to remove an item from empty LL, it should throw Exception");
+
+        }
+    }
+}
---
+++ Lab5/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Define a Node Class
+public class Node
+{
+    public int Data { get; set; }
+    public Node Next { get; set; }
+
+    public Node(int data)
+    {
+        Data = data;
+        Next = null;
+    }
+}
+
+// Define a Stack Class
+public class Stack
+{
+    private Node top;
+
+    public bool IsEmpty()
+    {
+        return top == null;
+    }
+
+    public void Push(int data)
+    {
+        Node newNode = new Node(data);
+        newNode.Next = top;
+        top = newNode;
+    }
+
+    public int Pop()
+    {
+        if (IsEmpty())
+        {
+            throw new InvalidOperationException("Stack is empty.");
+        }
+        int result = top.Data;
+        top = top.Next;
+        return result;
+    }
+
+    public void RemoveEverySecond()
+    {
+        if (top == null || top.Next == null) return;
+
+        Node current = top;
+        while (current != null && current.Next != null)
+        {
+            current.Next = current.Next.Next;
+            if (current.Next != null)
+                current = current.Next;
+        }
+    }
+
+    public void PrintStack()
+    {
+        Node current = top;
+        while (current != null)
+        {
+            Console.WriteLine(current.Data);
+            current = current.Next;
+        }
+        Console.WriteLine("-------------");
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        //Lab 1 and 3
+        // Stack Demonstration
+        Console.WriteLine("Stack Demonstration:");
+        // Create a Stack Object
+        Stack stack = new Stack();
+
+        // Push Elements
+        for (int i = 1; i <= 10; i++)
+        {
+            stack.Push(i);
+        }
+
+        // Print Initial Stack
+        Console.WriteLine("Initial Stack:");
+        stack.PrintStack();
+
+        // Remove Every Second Element
+        stack.RemoveEverySecond();
+
+        // Print Modified Stack
+        Console.WriteLine("After Removing Every Second Element:");
+        stack.PrintStack();
+
+        // Queue Demonstration
+        Console.WriteLine("Queue Demonstration:");
+        // Create a Queue Object
+        Queue<int> callerIds = new Queue<int>();
+
+        //Lab 2
+        // Enqueue Caller IDs
+        callerIds.Enqueue(101);
+        callerIds.Enqueue(102);
+        callerIds.Enqueue(103);
+        callerIds.Enqueue(104);
+        callerIds.Enqueue(105);
+
+        // Iterate Over the Queue
+        Console.WriteLine("List of Caller IDs in the Queue:");
+        foreach (int callerId in callerIds)
+        {
+            Console.WriteLine(callerId);
+        }
+    }
+}
